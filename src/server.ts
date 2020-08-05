564b381591b49ef4f9ee9dbd1ea72b6589ffae20
g-adler <guiadler@hotmail.com>
2020-08-05 11:50:26 -0300

    logging when service is up

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import routes from './routes';
 import cors from 'cors'
@@ -8,5 +7,7 @@ const app = express()
 app.use(cors)
 app.use(express.json())
 app.use(routes)
+
+console.log('\n Up N running dawg');
 
 app.listen(3333);
